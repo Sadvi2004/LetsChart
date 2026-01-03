748fec447d159e03392a624e847f6d220025c00b
sadvi134ash2004 <sadvibayyavarapu@gmail.com>
2026-01-03 13:00:45 +0530

    created frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+function App() {
+  return (
+    <div>
+      <p className=" text-3xl font-semibold text-center">Welcome to Let'sChat</p>
+    </div>
+  )
+}
+
+export default App
