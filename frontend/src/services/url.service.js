ec9b51cdac2e8444107c7769dafd333b94323b99
Sadvi2004 <sadvibayyavarapu@gmail.com>
2026-05-18 15:35:36 +0530

    changed code

--- frontend/src/services/url.service.js
+++ frontend/src/services/url.service.js
@@ -1,28 +1,9 @@
-import axios from "axios";
-
-// const apiUrl = `${import.meta.env.VITE_API_URL}/api`;
-const apiUrl = `https://letschart-2.onrender.com/api`;
-
-const getToken = () => localStorage.getItem("auth_token")
-
-const axiosInstance = axios.create({
-    baseURL: apiUrl,
-    withCredentials: true,
-});
-
-axiosInstance.interceptors.request.use((config) => {
-    const token = getToken();
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`
-    }
-    return config;
-})
-
-export default axiosInstance;
-
 // import axios from "axios";
 
-// const apiUrl = `${import.meta.env.VITE_API_URL}/api`;
+// // const apiUrl = `${import.meta.env.VITE_API_URL}/api`;
+// const apiUrl = `https://letschart-2.onrender.com/api`;
+
+// const getToken = () => localStorage.getItem("auth_token")
 
 // const axiosInstance = axios.create({
 //     baseURL: apiUrl,
@@ -30,13 +11,32 @@ export default axiosInstance;
 // });
 
 // axiosInstance.interceptors.request.use((config) => {
-//     const token = localStorage.getItem("auth_token");
-
+//     const token = getToken();
 //     if (token) {
-//         config.headers.Authorization = `Bearer ${token}`;
+//         config.headers.Authorization = `Bearer ${token}`
 //     }
-
 //     return config;
-// });
+// })
 
 // export default axiosInstance;
+
+import axios from "axios";
+
+const apiUrl = `https://letschart-2.onrender.com/api`;
+
+const axiosInstance = axios.create({
+    baseURL: apiUrl,
+    withCredentials: true,
+});
+
+axiosInstance.interceptors.request.use((config) => {
+    const token = localStorage.getItem("auth_token");
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+});
+
+export default axiosInstance;
